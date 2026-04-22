1602b4d0848d63f979f8a513cc69da238c90256d
prasadmpk45-lab <prasadmpk45@gmail.com>
2026-04-22 19:33:51 +0530

    initial IMS setus

---
+++ IMS-Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>IMS Dashboard</h1>
+      <p>Inventory Management System Frontend Template</p>
+    </div>
+  );
+}
+
+export default App;
